717e070183cd383f2a2207c10677038023a53077
Quang Pham <ptquang86@gmail.com>
2025-04-08 19:44:07 +0700

    Revert

--- my-calendar.py
+++ my-calendar.py
@@ -50,9 +50,9 @@ def on_error(e):
     logging.info(e)
 
 def go_to_sleep(epd):
-    logging.info("Clear...")
-    epd.init()
-    epd.Clear()
+    # logging.info("Clear...")
+    # epd.init()
+    # epd.Clear()
     logging.info("Goto Sleep...")
     epd.sleep()
 
@@ -81,13 +81,13 @@ def renderTasks(draw):
 
 try:
     logging.info("Starting...")
-    epd = epd4in2_V2.EPD()
-    logging.info("init and Clear")
-    epd.init()
-    epd.Clear()
-font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-    font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
-    font35 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 35)
+    epd = init()
+
+    fontBody = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
+    fontTitle = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+    fontHeadline = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 32)
+    # fontWeather = ImageFont.truetype(os.path.join(picdir, 'weathericons-regular-webfont.ttf'), 32)
+
     if 0:
         logging.info("E-paper refresh")
         epd.init()
@@ -103,20 +103,9 @@ font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
         epd.init_fast(epd.Seconds_1_5S)
         Himage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
         draw = ImageDraw.Draw(Himage)
-        draw.text((10, 0), 'hello world', font = font24, fill = 0)
-        draw.text((10, 20), '4.2inch V2 e-Paper', font = font24, fill = 0)
-        draw.text((150, 0), u'微雪电子', font = font24, fill = 0)
-        draw.line((20, 50, 70, 100), fill = 0)
-        draw.line((70, 50, 20, 100), fill = 0)
-        draw.rectangle((20, 50, 70, 100), outline = 0)
-        draw.line((165, 50, 165, 100), fill = 0)
-        draw.line((140, 75, 190, 75), fill = 0)
-        draw.arc((140, 50, 190, 100), 0, 360, fill = 0)
-        draw.rectangle((80, 50, 130, 100), fill = 0)
-        draw.chord((200, 50, 250, 100), 0, 360, fill = 0)
-        # renderDate(draw)
-        # renderWeather(draw)
-        # renderTasks(draw)
+        renderDate(draw)
+        renderWeather(draw)
+        renderTasks(draw)
         epd.display_Fast(epd.getbuffer(Himage))
         time.sleep(2)
 
