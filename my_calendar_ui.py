24dde7b704a6e7a24ea25f0330530962597e124e
Quang Pham <ptquang86@gmail.com>
2025-04-12 09:48:40 +0700

    Test anchor mm

--- my_calendar_ui.py
+++ my_calendar_ui.py
@@ -26,7 +26,9 @@ def renderOneLineList(draw, items, count):
         draw.line((0, itemPosition + CONFIG['listItem']['height'], viewport['width'], itemPosition + CONFIG['listItem']['height']), fill = FILL_BLACK)
         if showBorder:
             draw.rectangle((16, itemPosition + (CONFIG['listItem']['height'] - 16) / 2, 134, itemPosition + ((CONFIG['listItem']['height'] - 16) / 2) + 16), outline = FILL_BLACK)
-            draw.rectangle((viewport['width'] - CONFIG['listItem']['height'], itemPosition + (CONFIG['listItem']['height'] - 14) / 2, viewport['width'], itemPosition + ((CONFIG['listItem']['height'] - 14) / 2) + 14), outline = FILL_BLACK)
+            # draw.rectangle((viewport['width'] - CONFIG['listItem']['height'], itemPosition + (CONFIG['listItem']['height'] - 14) / 2, viewport['width'], itemPosition + ((CONFIG['listItem']['height'] - 14) / 2) + 14), outline = FILL_BLACK)
+            draw.line((viewport['width'] - CONFIG['listItem']['height'], itemPosition + (CONFIG['listItem']['height'] - 14) / 2, viewport['width'], itemPosition + (CONFIG['listItem']['height'] - 14) / 2), outline = FILL_BLACK)
+            draw.line((viewport['width'] - CONFIG['listItem']['height'] / 2, itemPosition, viewport['width'], itemPosition + CONFIG['listItem']['height']), outline = FILL_BLACK)
 
 def renderTwoLinesList(draw, items, count):
     for j in range(0, count):
