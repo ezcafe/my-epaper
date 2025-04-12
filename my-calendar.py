07160175d5a984636f46784aeec47d56cf74ecd5
Quang Pham <ptquang86@gmail.com>
2025-04-12 15:44:38 +0700

    Test time

--- my-calendar.py
+++ my-calendar.py
@@ -104,7 +104,7 @@ def renderEvents(draw):
     events = []
 
     start_time = datetime.datetime.now()
-    stop_time = datetime.datetime.now().timedelta(days=1,microseconds=-1)
+    stop_time = datetime.datetime.timedelta(days=1,microseconds=-1)
     logging.debug(f"Start time: {start_time.strftime('%Y-%m-%d %H:%M:%S')}")
     logging.debug(f"Stop time: {stop_time.strftime('%Y-%m-%d %H:%M:%S')}")
 
